ba26ada060193420ba8a4a893e2cac0e838449df
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2015-12-14 18:06:59 +0100

    yaml updated

--- web/main.dart
+++ web/main.dart
@@ -2,11 +2,9 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 import 'package:angular2/bootstrap.dart';
 
-import 'package:angular2/angular2.dart';
 import 'package:Dart_101/app_component.dart';
 import 'package:Dart_101/app_weather_component.dart';
 
-@View (directives: const [NgClass])
 main() {
   bootstrap(AppComponent);
   bootstrap(AppWeatherComponent);
